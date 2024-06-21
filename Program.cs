c2b6e6dbf3bf502dedea0e848cdd268b6e9bcfca
Raphael Rodrigues Marçal <104911531+OPhaels@users.noreply.github.com>
2024-06-21 19:31:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Calculos;
+using System;
+
+namespace Dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            conta res = new conta();
+            double dolar = 5.10; //valor do dólar
+
+            Console.WriteLine($"Valor da cotação do Dólar: {dolar}");
+            Console.Write("Quantos dólares deseja comprar? ");
+            float val = float.Parse(Console.ReadLine()); //capta a entrada do usuário
+         
+            res.ValorDolar = dolar;
+            res.ValorPagamento = val;
+            var valor = res.Resultado();
+            var resultadoFinal = res.porcentagem();
+
+            Console.WriteLine($"Valor a ser pago em reais contando com o IOF de 6% {resultadoFinal}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    class conta {
+        public double ValorDolar;
+        public double ValorPagamento;
+
+        public double Resultado()
+        {
+            return (ValorDolar * ValorPagamento);
+        }
+
+        public double porcentagem()
+        {
+            return Resultado() + ((Resultado() * 6) / 100);
+        }
+        
+    }
+}
